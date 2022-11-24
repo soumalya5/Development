5282aaa8cce112530df3f620f0f98d0c53c9f13e
Soumalya Mondal <soumalya.mondal6@gmail.com>
2022-11-24 16:01:06 +0530

    Update SampleDevCode.java

--- src/main/java/com/dev/web/SampleDevCode.java
+++ src/main/java/com/dev/web/SampleDevCode.java
@@ -3,6 +3,6 @@ package com.dev.web;
 public class SampleDevCode {
 
 	public static void main(String[] args) {
-		System.out.println("This is a sample development code ");
+		System.out.println("This is a sample development code in develpoment repo ");
 	}
 }
